b102b1d31cc23aa2278d25729509de9d58298ff8
DimaMukhin <dima9348@gmail.com>
2017-08-09 20:26:07 -0500

    index page React component

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+import Header from './components/header';
+
+const API_KEY = 'AIzaSyCEUHOwF0Lc6LNP8p8kRfmZw-pRS1kIvk8';
+
+// create new react component. should produce HTML
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
+      this.setState({ videos, selectedVideo: videos[0] });
+    });
+  }
+
+  render() {
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
+    return (
+      <div>
+        <Header />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+// insert generated HTML in DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
